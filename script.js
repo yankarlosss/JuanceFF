c788903907b280a1bf56d3ae1bbe44a461961be1
yankarlosss <yankarlosssss@gmail.com>
2025-05-14 01:19:30 -0500

    script.js

--- script.js
+++ script.js
@@ -7,12 +7,18 @@ for (let i = 0; i < 25; i++) {
   document.body.appendChild(d);
 }
 
-// Mostrar botón
+// Mostrar botón y mensaje de alerta
 function mostrarBoton() {
   document.getElementById('abrirBtn').style.display = 'inline-block';
+
+  // Detectar si está en TikTok, Facebook o Instagram
+  const agente = navigator.userAgent.toLowerCase();
+  if (agente.includes("tiktok") || agente.includes("fb") || agente.includes("instagram")) {
+    document.getElementById('alerta').style.display = 'block';
+  }
 }
 
-// Redireccionar a navegador
+// Redireccionar a navegador externo
 function abrirNavegador() {
-  window.location.href = "https://diamantes-pall-free.blogspot.com/"; // <- CAMBIA por tu URL real
+  window.location.href = "https://tusitio.com"; // Reemplaza por tu URL real
 }
