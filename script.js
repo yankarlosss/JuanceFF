8a9b285d8749aa02589606271896cade95e47af2
yankarlosss <yankarlosssss@gmail.com>
2025-05-14 00:57:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Crear diamantes flotantes
+for (let i = 0; i < 25; i++) {
+  const d = document.createElement('div');
+  d.classList.add('diamante');
+  d.style.left = Math.random() * 100 + 'vw';
+  d.style.animationDuration = (5 + Math.random() * 10) + 's';
+  document.body.appendChild(d);
+}
+
+// Mostrar botón
+function mostrarBoton() {
+  document.getElementById('abrirBtn').style.display = 'inline-block';
+}
+
+// Redireccionar a navegador
+function abrirNavegador() {
+  window.location.href = "https://tusitio.com"; // <- CAMBIA por tu URL real
+}
